cb29608ca3f397bab66f1e77ad175e44eb786283
Roman Ovsepian <roman.ovsepian@extrawest.com>
2022-12-05 15:31:12 +0200

    Create assets.dart

---
+++ lib/assets.dart
@@ -0,0 +1,3 @@
+const fortuneWheelTitle = 'assets/images/fortune_wheel_title.png';
+const spinButton = 'assets/images/spin_button.png';
+const spinButtonPressed = 'assets/images/spin_button_pressed.png';
