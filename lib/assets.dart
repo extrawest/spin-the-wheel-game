b9f68955d0033fe995842d5c60a0f0ea70f335df
Roman Ovsepian <roman.ovsepian@extrawest.com>
2022-12-06 11:40:50 +0200

    add filled wheel asset

--- lib/assets.dart
+++ lib/assets.dart
@@ -2,4 +2,5 @@ const fortuneWheelTitle = 'assets/images/fortune_wheel_title.png';
 const spinButton = 'assets/images/spin_button.png';
 const spinButtonPressed = 'assets/images/spin_button_pressed.png';
 const spinningWheel = 'assets/images/spin_wheel.png';
+const spinningWheelFilled = 'assets/images/spin_wheel_filled.png';
 const spinningPointer = 'assets/images/pointer.png';
--- lib/widgets/spin_wheel_game.dart
+++ lib/widgets/spin_wheel_game.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinning_wheel/flutter_spinning_wheel.dart';
 import 'package:provider/provider.dart';
 import 'package:spin_wheel_game/assets.dart';
 import 'package:spin_wheel_game/spin_wheel_cubit/spin_wheel_cubit.dart';
+import 'package:spin_wheel_game/utils.dart';
 import 'package:spin_wheel_game/widgets/spin_button.dart';
+
+
+const _horizontalPaddingValue = 24.0;
 
 class SpinWheelGame extends StatefulWidget {
   const SpinWheelGame({Key? key}) : super(key: key);
@@ -41,7 +44,7 @@ class _SpinWheelGameContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      padding: const EdgeInsets.symmetric(horizontal: _horizontalPaddingValue),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -53,17 +56,19 @@ class _SpinWheelGameContent extends StatelessWidget {
               border: Border.all(color: Colors.black),
             ),
             child: SpinningWheel(
-              Image.asset(spinningWheel, width: 500),
-              width: 300,
-              height: 300,
+              Image.asset(spinningWheelFilled),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width - (_horizontalPaddingValue * 2),
               dividers: 7,
               canInteractWhileSpinning: false,
               secondaryImage: Image.asset(spinningPointer),
               secondaryImageHeight: 50,
               secondaryImageWidth: 50,
               secondaryImageTop: 0,
+              secondaryImageLeft: MediaQuery.of(context).size.width / 2 - _horizontalPaddingValue * 2,
               shouldStartOrStop: context.read<StreamController<double>>().stream,
-              spinResistance: 0.05,
+              spinResistance: 0.1,
+              initialSpinAngle: generateRandomAngle(),
               onEnd: (value) {
                 context.read<SpinWheelCubit>().setIsSpinning(false);
               },
