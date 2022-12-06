f6b0bea041bfd2c5e77fa0bc4fd22b3f90611c50
Roman Ovsepian <roman.ovsepian@extrawest.com>
2022-12-06 09:38:02 +0200

    Update spin_wheel.png

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,6 +1,5 @@
 const fortuneWheelTitle = 'assets/images/fortune_wheel_title.png';
 const spinButton = 'assets/images/spin_button.png';
 const spinButtonPressed = 'assets/images/spin_button_pressed.png';
-const spinningWheel = 'assets/images/spinning_wheel.png';
-const spinningWheel2 = 'assets/images/spin_wheel.png';
+const spinningWheel = 'assets/images/spin_wheel.png';
 const spinningPointer = 'assets/images/pointer.png';
