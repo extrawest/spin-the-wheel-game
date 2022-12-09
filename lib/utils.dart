f9bb7ad26208f36341d7c66a40e35933c874907a
Roman Ovsepian <roman.ovsepian@extrawest.com>
2022-12-09 16:00:27 +0200

    create prize_type.dart, replace prize name with it

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,13 +1,14 @@
 import 'package:spin_wheel_game/assets.dart';
 import 'package:spin_wheel_game/models/lottie_type.dart';
 import 'package:spin_wheel_game/models/prize.dart';
+import 'package:spin_wheel_game/models/prize_type.dart';
 
 final Map<int, Prize> prizes = {
-  0: Prize(asset: heart, multiplier: 1, name: 'live', lottie: LottieType.common),
-  1: Prize(asset: crown, multiplier: 1, name: 'jackpot', lottie: LottieType.golden),
-  2: Prize(asset: diamond, multiplier: 1, name: 'diamond', lottie: LottieType.coins),
-  3: Prize(asset: heart, multiplier: 4, name: 'lives', lottie: LottieType.common),
-  4: Prize(asset: flash, multiplier: 4, name: 'powers', lottie: LottieType.common),
-  5: Prize(asset: dollar, multiplier: 2, name: 'dollars', lottie: LottieType.coins),
-  6: Prize(asset: flash, multiplier: 3, name: 'powers', lottie: LottieType.common),
+  0: Prize(asset: heart, multiplier: 1, prizeType: PrizeType.heart, lottie: LottieType.common),
+  1: Prize(asset: darkCrown, multiplier: 1, prizeType: PrizeType.jackpot, lottie: LottieType.golden),
+  2: Prize(asset: diamond, multiplier: 1, prizeType: PrizeType.diamond, lottie: LottieType.coins),
+  3: Prize(asset: heart, multiplier: 4, prizeType: PrizeType.heart, lottie: LottieType.common),
+  4: Prize(asset: flash, multiplier: 4, prizeType: PrizeType.energy, lottie: LottieType.common),
+  5: Prize(asset: dollar, multiplier: 2, prizeType: PrizeType.dollar, lottie: LottieType.coins),
+  6: Prize(asset: flash, multiplier: 3, prizeType: PrizeType.energy, lottie: LottieType.common),
 };
